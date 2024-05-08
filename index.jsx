048b0e1e1a68904248136c728cab663ebb480eea
katarighe <m.ighe@hotmail.com>
2024-05-08 11:20:12 +0300

    Fix linter errors

--- index.jsx
+++ index.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 
 function App() {
-    return (
-        <div className="App">
-            <h1>Hello, React!</h1>
-            <p>Welcome to my sample React file.</p>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>Hello, React!</h1>
+      <p>Welcome to my sample React file.</p>
+    </div>
+  );
 }
 
 export default App;
