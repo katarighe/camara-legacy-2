bf0b64b48edb36cab6ac6bd2273456ce2906d70d
katarighe <m.ighe@hotmail.com>
2024-05-08 11:18:42 +0300

    Add dummy files in order to pass the checks

---
+++ index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello, React!</h1>
+            <p>Welcome to my sample React file.</p>
+        </div>
+    );
+}
+
+export default App;
