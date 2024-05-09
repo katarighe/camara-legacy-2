792d1953158e36fde3fc6061c59374fabfee8fe9
katarighe <m.ighe@hotmail.com>
2024-05-09 16:51:30 +0300

    Add content on the About Us page

---
+++ src/components/About/About.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./About.css";
+import about from "../../assets/images/about.jpg";
+import play_icon from "../../assets/images/play-icon.png";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about-left">
+        <img src={about} alt="" />
+        <img src={play_icon} alt="" />
+      </div>
+      <div className="about-right">
+        <h3>About Camara Legacy Sdn Bhd</h3>
+        <h2>Importing Excellence, Exporting Essence</h2>
+        <p>
+          At Camara Legacy, we blend tradition with modernity to offer a diverse
+          array of services rooted in African heritage. With a commitment to
+          integrity and excellence, we specialize in facilitating import and
+          export transactions, sourcing and selling raw African wild honey, and
+          providing wholesale furniture sales that showcase the rich
+          craftsmanship of the continent.
+          <p>
+            Our mission is to bridge cultures, promote sustainability, and
+            foster meaningful connections with partners and customers worldwide.
+          </p>
+        </p>
+        <p>
+          Camara Legacy is dedicated to delivering quality products and services
+          while upholding ethical business practices and environmental
+          responsibility.
+        </p>
+        <p>
+          Through our offerings, we invite you to experience the essence of
+          Africa and join us in writing the next chapter of our enduring legacy.
+          Contact us today to explore how we can collaborate and make a positive
+          impact together.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
