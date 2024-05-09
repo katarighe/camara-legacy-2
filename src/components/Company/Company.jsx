434a874e9782d95c904e47293ce89ef297e0d7a7
katarighe <m.ighe@hotmail.com>
2024-05-09 20:08:12 +0300

    Create CSS file for Company

---
+++ src/components/Company/Company.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Company.css'
+
+const Company = () => {
+  return (
+    <div className='company'>
+      <div className="gallery">
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Company;
