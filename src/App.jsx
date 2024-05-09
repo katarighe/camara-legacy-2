76769aaccff0a7b1de635dd393a7d09dd5bbfe33
katarighe <m.ighe@hotmail.com>
2024-05-09 15:04:34 +0300

    Add Services component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
 import Hero from './components/Hero/Hero.jsx'
+import Services from './components/Services/Services.jsx'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Services />
     </div>
   );
 }
