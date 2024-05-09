73e5d900ffa2ce24c6bd8edfaaca52c117380c75
katarighe <m.ighe@hotmail.com>
2024-05-09 16:11:24 +0300

    Add proptypes

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import './Title.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Title.css';
 
-const Title = ({subTitle, title}) => {
+const Title = ({ subTitle, title }) => {
   return (
     <div className='title'>
       <p>{subTitle}</p>
       <h2>{title}</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Title
+Title.propTypes = {
+  subTitle: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default Title;
