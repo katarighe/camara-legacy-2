22b6c0af934f5f989afda1d67c70bb6faa0580b8
katarighe <m.ighe@hotmail.com>
2024-05-09 19:23:50 +0300

    Add functions that make the Navbar transpartent and color between scrolling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/images/logo.svg";
 
 const Navbar = () => {
+
+  const [sticky, setSticky] = useState(false);
+    useEffect(()=>{
+      window.addEventListener('scroll', () => {
+        window.scrollY > 50 ? setSticky(true) : setSticky(false);
+      })
+    }, []);
+  
   return (
-    <nav className="container color-nav">
+    <nav className={`container ${sticky? 'color-nav' : ''}`}>
       <img src={logo} alt="" className="logo" />
       <ul>
         <li>
