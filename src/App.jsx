a903106de4a24bb1e737cfadd818e2c5a025af8c
katarighe <m.ighe@hotmail.com>
2024-05-09 11:43:11 +0300

    Minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
-import Hero from './components/Hero/Hero.jsx';
+import Hero from './components/Hero/Hero.jsx'
 
 const App = () => {
   return (
