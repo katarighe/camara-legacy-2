29b386ba4fca34b1afd68796325e145d9580bcfe
katarighe <m.ighe@hotmail.com>
2024-05-09 17:03:53 +0300

    Update the paragraphs

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,23 +7,25 @@ const About = () => {
   return (
     <div className="about">
       <div className="about-left">
-        <img src={about} alt="" className="about-img"/>
-        <img src={play_icon} alt="" className="play-icon"/>
+        <img src={about} alt="" className="about-img" />
+        <img src={play_icon} alt="" className="play-icon" />
       </div>
       <div className="about-right">
         <h3>About Us</h3>
         <h2>Importing Excellence, Exporting Essence</h2>
         <p>
           At Camara Legacy Sdn Bhd, we blend tradition with modernity to offer a
-          diverse array of services rooted in African heritage. With a
-          commitment to integrity and excellence, we specialize in facilitating
-          import and export transactions, sourcing and selling raw African wild
-          honey, and providing wholesale furniture sales that showcase the rich
-          craftsmanship of the continent.
-          <p>
-            Our mission is to bridge cultures, promote sustainability, and
-            foster meaningful connections with partners and customers worldwide.
-          </p>
+          diverse array of services rooted in African heritage.{" "}
+        </p>
+        <p>
+          With a commitment to integrity and excellence, we specialize in
+          facilitating import and export transactions, sourcing and selling raw
+          African wild honey, and providing wholesale furniture sales that
+          showcase the rich craftsmanship of the continent.
+        </p>
+        <p>
+          Our mission is to bridge cultures, promote sustainability, and foster
+          meaningful connections with partners and customers worldwide.
         </p>
         <p>
           Camara Legacy Sdn Bhd is dedicated to delivering quality products and
