54bdd0ef6ae2721988b6a333c9e6238ff02e53c3
katarighe <m.ighe@hotmail.com>
2024-05-09 12:07:31 +0300

    Add the text for the website

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import "./Hero.css";
 
 const Hero = () => {
-  return <div className="hero"></div>;
+  return (
+    <div className="hero container">
+      <div className="hero-text">
+        <h1>Connecting the world through trade and nature's treasures.</h1>
+        <p>
+          We're passionate about facilitating seamless import-export operations
+          and crafting exquisite Raw African Wild Honey. Our company serves as a
+          hub for innovation, collaboration, and transparency as we strive to
+          leave a lasting legacy in global commerce and conservation. Join us in
+          exploring cutting-edge solutions and sustainable practices that define
+          the essence of Camara Legacy.
+        </p>
+        <button className="btn">Explore more</button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
