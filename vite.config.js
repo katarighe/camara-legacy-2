0cad45ebe4257b1dbcf1e1304d609e3d2d234076
katarighe <m.ighe@hotmail.com>
2024-05-10 11:38:44 +0300

    Remove comments

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // Ensure that Vite resolves SVG imports correctly
-      // Adjust the path if necessary
-      '@assets': 'src/assets', // Adjusted path without leading slash
+      '@assets': 'src/assets',
     },
   },
-  assetsInclude: ['**/*.JPG'], // Add assetsInclude option to handle JPG files
+  assetsInclude: ['**/*.JPG'],
 });
