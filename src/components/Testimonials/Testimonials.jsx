e653ffab1b0d08fed2657e9a3c7330295d31b611
katarighe <m.ighe@hotmail.com>
2024-05-10 18:07:54 +0300

    Add the JS function for the slides

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Testimonials.css";
 import next_icon from "../../assets/images/next-icon.png";
 import back_icon from "../../assets/images/back-icon.png";
@@ -8,85 +8,108 @@ import user_3 from "../../assets/images/user-3.png";
 import user_4 from "../../assets/images/user-4.png";
 
 const Testimonials = () => {
+  const slider = useRef();
+  let tx = 0;
+
+  const slideForward = () => {
+    if (tx > -50) {
+      tx -= 25;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`;
+  };
+
+  const slideBackward = () => {
+    if (tx < 0) {
+      tx += 25;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`;
+  };
+
   return (
     <div className="testimonials">
-      <img src={next_icon} alt="" className="next-btn" />
-      <img src={back_icon} alt="" className="back-btn" />
-      <div className="slider"></div>
-      <ul>
-        <li>
-          <div className="slide">
-            <div className="user-info">
-              <div>
-                <img src={user_1} />
-                <h3>Laura</h3>
-                <span>Kuala Lumpur</span>
+      <img src={next_icon} alt="" className="next-btn" onClick={slideForward} />
+      <img
+        src={back_icon}
+        alt=""
+        className="back-btn"
+        onClick={slideBackward}
+      />
+      <div className="slider">
+        <ul ref={slider}>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <div>
+                  <img src={user_1} alt="" />
+                  <h3>Laura</h3>
+                  <span>Kuala Lumpur</span>
+                </div>
               </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed
+                nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis
+                ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.
+                Mauris massa.
+              </p>
             </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-              nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-              Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
-              Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris
-              massa.
-            </p>
-          </div>
-        </li>
-        <li>
-          <div className="slide">
-            <div className="user-info">
-              <div>
-                <img src={user_2} />
-                <h3>Maurice</h3>
-                <span>Sri Kembangan</span>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <div>
+                  <img src={user_2} alt="" />
+                  <h3>Maurice</h3>
+                  <span>Sri Kembangan</span>
+                </div>
               </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed
+                nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis
+                ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.
+                Mauris massa.
+              </p>
             </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-              nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-              Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
-              Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris
-              massa.
-            </p>
-          </div>
-        </li>
-        <li>
-          <div className="slide">
-            <div className="user-info">
-              <div>
-                <img src={user_3} />
-                <h3>John Doe</h3>
-                <span>Petaling Jaya</span>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <div>
+                  <img src={user_3} alt="" />
+                  <h3>John Doe</h3>
+                  <span>Petaling Jaya</span>
+                </div>
               </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed
+                nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis
+                ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.
+                Mauris massa.
+              </p>
             </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-              nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-              Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
-              Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris
-              massa.
-            </p>
-          </div>
-        </li>
-        <li>
-          <div className="slide">
-            <div className="user-info">
-              <div>
-                <img src={user_4} />
-                <h3>John Doe</h3>
-                <span>Wangsa Maju</span>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <div>
+                  <img src={user_4} alt="" />
+                  <h3>John Doe</h3>
+                  <span>Wangsa Maju</span>
+                </div>
               </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed
+                nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis
+                ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.
+                Mauris massa.
+              </p>
             </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-              nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-              Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
-              Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris
-              massa.
-            </p>
-          </div>
-        </li>
-      </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
