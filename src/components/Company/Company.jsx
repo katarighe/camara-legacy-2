99bd82a11db316e2936433436de3e61193ebed34
katarighe <m.ighe@hotmail.com>
2024-05-10 11:41:34 +0300

    Add the images to the gallery

--- src/components/Company/Company.jsx
+++ src/components/Company/Company.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import './Company.css'
+import gallery_1 from '../../assets/images/gallery-1.JPG'
+import gallery_2 from '../../assets/images/gallery-2.jpg'
+import gallery_3 from '../../assets/images/gallery-3.JPG'
+import gallery_4 from '../../assets/images/gallery-4.jpg'
 
 const Company = () => {
   return (
     <div className='company'>
       <div className="gallery">
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
+        <img src={gallery_1} alt="" />
+        <img src={gallery_2} alt="" />
+        <img src={gallery_3} alt="" />
+        <img src={gallery_4} alt="" />
       </div>
     </div>
   )
