dfabb893e3fe2ccb82dc9d8386cb9e9ccebc01a3
katarighe <m.ighe@hotmail.com>
2024-05-11 14:01:17 +0300

    Add  bolding

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Contact.css";
-import msg_icon from '../../assets/images/msg-icon.png'
+import msg_icon from "../../assets/images/msg-icon.png";
 
 const Contact = () => {
   return (
     <div className="contact">
       <div className="contact-col">
-        <h3>Send Us A Message <img src={msg_icon} alt="" /></h3>
+        <h3>
+          Send Us A Message <img src={msg_icon} alt="" />
+        </h3>
         <p>
           Welcome to Camara Legacy! We specialize in import/export services,
           offering premium raw African wild honey and wholesale furniture.
@@ -16,11 +18,16 @@ const Contact = () => {
           details below. We look forward to hearing from you!
         </p>
         <ul>
-            <li>info@camara-legacy.com.my</li>
-            <li>+60 17-542 3230 (Malaysia)</li>
-            <li>+224 610 23 92 31 (Guinea)</li>
-            <li>104, Jln 1, Taman Industri Bolton, 68100 Batu Caves, Selangor, Malaysia</li>
-            <li>Opening Hours: Monday to Sunday from 10 AM to 6 PM</li>
+          <li>info@camara-legacy.com.my</li>
+          <li>+60 17-542 3230 (Malaysia)</li>
+          <li>+224 610 23 92 31 (Guinea)</li>
+          <li>
+            104, Jln 1, Taman Industri Bolton, 68100 Batu Caves, Selangor,
+            Malaysia
+          </li>
+          <li>
+            <b>Opening Hours:</b> Monday to Sunday from 10 AM to 6 PM
+          </li>
         </ul>
       </div>
       <div className="contact-col"></div>
