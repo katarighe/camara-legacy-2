ab0b10162ef2d2e8307a7566180fd3532bc36665
katarighe <m.ighe@hotmail.com>
2024-05-12 12:28:50 +0300

    Add the Copyright Year and Social Media

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,13 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-      
+      <p>© 2024 Camara Legacy</p>
+      <ul>
+        <li>Facebook</li>
+        <li>Instagram</li>
+        <li>YouTube</li>
+        <li>Twitter</li>
+      </ul>
     </div>
   )
 }
