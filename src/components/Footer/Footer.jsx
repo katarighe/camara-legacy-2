1772f20b2addd13299c95fdb21c64e4ef3cb954d
katarighe <m.ighe@hotmail.com>
2024-05-12 12:37:03 +0300

    Arrange the lines

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import { SocialIcon } from 'react-social-icons'
-import './Footer.css'
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className='footer'>
+    <div className="footer">
       <p>© 2024 Camara Legacy Sdn Bhd</p>
       <ul>
-        <li><SocialIcon url="https://facebook.com" /></li>
-        <li><SocialIcon url="https://instagram.com" /></li>
-        <li><SocialIcon url="https://youtube.com" /></li>
-        <li><SocialIcon url="https://twitter.com" /></li>
+        <li>
+          <SocialIcon url="https://facebook.com" />
+        </li>
+        <li>
+          <SocialIcon url="https://instagram.com" />
+        </li>
+        <li>
+          <SocialIcon url="https://youtube.com" />
+        </li>
+        <li>
+          <SocialIcon url="https://twitter.com" />
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
