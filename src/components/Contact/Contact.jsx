176b0f271f364fcb548694921a643582ce01fcf5
katarighe <m.ighe@hotmail.com>
2024-05-12 11:56:18 +0300

    Organize the text

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Contact.css";
 import msg_icon from "../../assets/images/msg-icon.png";
-import mail_icon from '../../assets/images/mail-icon.png';
-import phone_icon from '../../assets/images/phone-icon.png';
-import location_icon from '../../assets/images/location-icon.png';
-import clock_icon from '../../assets/images/clock-icon.png';
-import white_arrow from '../../assets/images/white-arrow.png';
+import mail_icon from "../../assets/images/mail-icon.png";
+import phone_icon from "../../assets/images/phone-icon.png";
+import location_icon from "../../assets/images/location-icon.png";
+import clock_icon from "../../assets/images/clock-icon.png";
+import white_arrow from "../../assets/images/white-arrow.png";
 
 const Contact = () => {
   const [result, setResult] = React.useState("");
@@ -19,7 +19,7 @@ const Contact = () => {
 
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
-      body: formData
+      body: formData,
     });
 
     const data = await response.json();
@@ -48,26 +48,54 @@ const Contact = () => {
           details below. We look forward to hearing from you!
         </p>
         <ul>
-          <li> <img src={mail_icon} /> info@camara-legacy.com.my</li>
-          <li> <img src={phone_icon} /> +60 17 542 3230 (Malaysia) <br /> +224 610 23 92 31 (Guinea)</li> 
-          <li> <img src={location_icon} />
+          <li>
+            {" "}
+            <img src={mail_icon} /> info@camara-legacy.com.my
+          </li>
+          <li>
+            {" "}
+            <img src={phone_icon} /> +60 17 542 3230 (Malaysia) <br /> +224 610
+            23 92 31 (Guinea)
+          </li>
+          <li>
+            {" "}
+            <img src={location_icon} />
             104, Jln 1, Taman Industri Bolton, 68100 Batu Caves, Selangor,
             Malaysia
           </li>
-          <li> <img src={clock_icon} />
+          <li>
+            {" "}
+            <img src={clock_icon} />
             <b>Opening Hours:</b> 10 AM to 6 PM (Monday to Sunday)
           </li>
         </ul>
       </div>
       <div className="contact-col">
         <form onSubmit={onSubmit}>
-            <label>Name</label>
-            <input type="text" name='name' placeholder='Enter your name' required/>
-            <label>Phone Number</label>
-            <input type="tel" name='phone' placeholder='Enter your mobile number' required/>
-            <label>Message</label>
-            <textarea name="message" rows="6" placeholder='Enter your message here' required></textarea>
-            <button type='submit' className="btn dark-btn">Submit Form <img src={white_arrow} /></button>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            required
+          />
+          <label>Phone Number</label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Enter your mobile number"
+            required
+          />
+          <label>Message</label>
+          <textarea
+            name="message"
+            rows="6"
+            placeholder="Enter your message here"
+            required
+          ></textarea>
+          <button type="submit" className="btn dark-btn">
+            Submit Form <img src={white_arrow} />
+          </button>
         </form>
         <br />
         <span>{result}</span>
